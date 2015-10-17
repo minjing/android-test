e656aadf459f35e9b9b5ceb3b45a262f844ec6f4
minjing <min_jing@aliyun.com>
2015-10-17 21:31:49 +0800

    Add fragment example

---
+++ FragmentExample/app/src/main/java/example/com/fragmentexample/FragmentDigital.java
@@ -0,0 +1,20 @@
+package example.com.fragmentexample;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by min on 15/10/17.
+ */
+public class FragmentDigital extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_digital, container, false);
+    }
+}
--- FragmentExample/app/src/main/java/example/com/fragmentexample/MainActivity.java
+++ FragmentExample/app/src/main/java/example/com/fragmentexample/MainActivity.java
@@ -1,6 +1,8 @@
-package example.com.stonehengeguide;
+package example.com.fragmentexample;
 
-import android.content.Intent;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -9,23 +11,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView mainImageView;
-        mainImageView = (ImageView) findViewById(R.id.imageView);
-        mainImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(getApplicationContext(),
-                        ImageActivity.class));
-            }
-        });
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -37,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        analogButton = (Button) findViewById(R.id.button_analog);
+        analogButton.setOnClickListener(this);
+        digitalButton = (Button) findViewById(R.id.button_digital);
+        digitalButton.setOnClickListener(this);
     }
 
     @Override
@@ -59,5 +57,29 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private Button analogButton, digitalButton;
+
+    @Override
+    public void onClick(View view) {
+        Fragment fragment;
+        if (view == analogButton) {
+            fragment = new FragmentAnalog();
+        } else {
+            fragment = new FragmentDigital();
+        }
+        replaceFragment(fragment);
+    }
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager manager = getFragmentManager();
+        FragmentTransaction transaction =
+                manager.beginTransaction();
+        transaction.setTransition(
+                FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        transaction.replace(R.id.fragment_container,fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
